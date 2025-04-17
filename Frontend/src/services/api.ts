4ae0c465a80e7bc5acd51f4ce8777337041d4c67
mandeotv1234 <22120201@student.hcmus.edu.vn>
2025-04-18 00:14:00 +0700

    fix domain api backend

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // change if your backend uses another port
+  baseURL: 'https://g-scores-backend-g1i4.onrender.com/api', // change if your backend uses another port
 });
 
 export default api;
